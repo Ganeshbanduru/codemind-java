5d66050847be9cc1a2d074c91e68bee38f20c20d
Ganeshbanduru <102582679+Ganeshbanduru@users.noreply.github.com>
2022-10-27 00:25:31 +0530

    import java.util.*; class array{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt(),count=0,avg=0,o=0;         int a[]=new int[n];         for (int i=0;i<n;i++)             a[i]=sc.nextInt();         for(int i:a)             o=o+i;         avg=o/n;         for(int j:a){             if(j>=avg)                 count++;         }             System.out.println(count);
        }
    }

---
+++ elements_greater_than_average.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class array{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),count=0,avg=0,o=0;
+        int a[]=new int[n];
+        for (int i=0;i<n;i++)
+            a[i]=sc.nextInt();
+        for(int i:a)
+            o=o+i;
+        avg=o/n;
+        for(int j:a){
+            if(j>=avg)
+                count++;
+        }
+            System.out.println(count);
+        
+        
+    }
+}
