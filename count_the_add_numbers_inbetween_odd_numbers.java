4e63f1cb52b3f03fceaa5694826867df89395c9c
Ganeshbanduru <102582679+Ganeshbanduru@users.noreply.github.com>
2022-10-26 22:53:40 +0530

    import java.util.*; class array{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt(),count=0;         int gani[]=new int[n];         for(int i=0;i<n;i++)             gani[i]=sc.nextInt();         for(int j=1;j<n-1;j++){             if(gani[j]%2!=0 && gani[j-1]%2!=0 && gani[j+1]%2!=0){                 count++;             }         }         System.out.println(count);     } }

---
+++ count_the_add_numbers_inbetween_odd_numbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class array{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),count=0;
+        int gani[]=new int[n];
+        for(int i=0;i<n;i++)
+            gani[i]=sc.nextInt();
+        for(int j=1;j<n-1;j++){
+            if(gani[j]%2!=0 && gani[j-1]%2!=0 && gani[j+1]%2!=0){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
