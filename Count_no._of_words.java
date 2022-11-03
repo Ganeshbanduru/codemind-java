406cfac4b9a952cfb59ae29887f123ddc6c41f2c
Ganeshbanduru <102582679+Ganeshbanduru@users.noreply.github.com>
2022-11-04 00:26:04 +0530

    import java.util.Scanner; class String1 {
    	public static void main(String[] args) {
    		Scanner sc=new Scanner(System.in);
    		String s1=sc.nextLine();
    		int c=0;
    		for(int i=0;i<s1.length();i++){
    		    char ch=s1.charAt(i);
    		    if(ch==' ')
    		        c++;
    		}
    		System.out.println(c+1);
    		
    	}
    
    }

---
+++ Count_no._of_words.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class String1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.nextLine();
+		int c=0;
+		for(int i=0;i<s1.length();i++){
+		    char ch=s1.charAt(i);
+		    if(ch==' ')
+		        c++;
+		}
+		System.out.println(c+1);
+		
+	}
+
+}
