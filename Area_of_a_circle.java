b8938461d3cc09bbaf075f0d0dc74f665fd6d14b
Ganeshbanduru <102582679+Ganeshbanduru@users.noreply.github.com>
2023-02-03 17:33:19 +0530

    import java.util.*; class Demo{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int r=sc.nextInt();         float k=3.14f*r*r;         System.out.format("%.2f",k);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Demo{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        float k=3.14f*r*r;
+        System.out.format("%.2f",k);
+    }
+}
