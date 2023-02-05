487efe8637c7655a45ca12a9926344a35fb13e30
Ganeshbanduru <102582679+Ganeshbanduru@users.noreply.github.com>
2023-02-05 16:25:03 +0530

    import java.util.*; class unique{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int num=sc.nextInt();         int temp=num,rem=0,c,t,r,flag=0;         while(temp!=0){             rem=temp%10;             c=0;             t=num;             while(t!=0){                 r=t%10;                 if(r==rem){                     c++;                 }                 if(c>1){                     flag=1;                     break;                 }                 t/=10;             }             temp/=10;         }         if(flag==0)             System.out.println("Unique Number");         else             System.out.println("Not Unique Number");     } }

--- Unique_number.java
+++ Unique_number.java
@@ -1,35 +1,29 @@
-import java.util.Scanner;
-class ganesh {
-	    public static void main(String args[]){
-	        Scanner sc=new Scanner(System.in);
-	        int temp,num,rem,c=0,t,r,f=0;
-	        num=sc.nextInt();
-	        temp=num;
-	        while(temp!=0)
-	        {
-	            rem=temp%10;
-	            c=0;
-	            t=num;
-	            while(t!=0)
-	            {
-	               r=t%10;
-	               if(r==rem)
-	               {
-	                   c++;
-	               }
-	               if(c>1)
-	               {
-	                   f=1;
-	                   break;
-	               }
-	               t=t/10;
-	            }
-	            temp=temp/10;
-	            
-	        }
-	        if(f==0)
-	        System.out.println("Unique Number");
-	        else
-	        System.out.println("Not Unique Number");
-	    }
-	}
+import java.util.*;
+class unique{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int temp=num,rem=0,c,t,r,flag=0;
+        while(temp!=0){
+            rem=temp%10;
+            c=0;
+            t=num;
+            while(t!=0){
+                r=t%10;
+                if(r==rem){
+                    c++;
+                }
+                if(c>1){
+                    flag=1;
+                    break;
+                }
+                t/=10;
+            }
+            temp/=10;
+        }
+        if(flag==0)
+            System.out.println("Unique Number");
+        else
+            System.out.println("Not Unique Number");
+    }
+}
