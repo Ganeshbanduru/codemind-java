cbc27a8a538cdf911e9690267c4187c9a3596e17
Ganeshbanduru <102582679+Ganeshbanduru@users.noreply.github.com>
2023-08-25 17:03:31 +0530

    import java.util.Scanner; public class count {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,sum=0,m,temp=0,c=0;         n=sc.nextInt();         m=sc.nextInt();         for(int i=n;i<=m;i++)         {             c=0;             sum=0;             temp=i;             while(temp>0)             {             int d=temp%10;             c++;             if( d==0)             break;             else if(i%d==0)             sum++;             temp/=10;             }             if(c==sum)              System.out.print(i+" ");         }     } }

--- Self_Dividing_Numbers.java
+++ Self_Dividing_Numbers.java
@@ -1,30 +1,29 @@
-import java.util.*;
-public class Oostive{
-    public static boolean Postive(int n){
-        int r=0,c=0,flag=0;
-            int k=n;
-            while(k>0){
-                r=k%10;
-                c++;
-                if(r==0)
-                    break;
-                else if(n%r==0)
-                    flag++;
-                k/=10;
-            }
-            if(flag==c){
-                return true;
-            }
-            else 
-                return false;
-        
-        }
-    public static void main(String args[]){
+import java.util.Scanner;
+public class count
+{
+    public static void main(String args[])
+    {
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int m=sc.nextInt();
+        int n,sum=0,m,temp=0,c=0;
+        n=sc.nextInt();
+        m=sc.nextInt();
         for(int i=n;i<=m;i++)
-            if(Postive(i)==true)
-                System.out.print(i+" ");
+        {
+            c=0;
+            sum=0;
+            temp=i;
+            while(temp>0)
+            {
+            int d=temp%10;
+            c++;
+            if( d==0)
+            break;
+            else if(i%d==0)
+            sum++;
+            temp/=10;
+            }
+            if(c==sum)
+             System.out.print(i+" ");
+        }
     }
 }
